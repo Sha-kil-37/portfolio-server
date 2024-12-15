a70dcd5555dab3b2c93e2d2579f2cd47f45f8ba6
Shakil <sakildevmern@gmail.com>
2024-12-15 17:25:08 +0600

    112/15/2024

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const fastify = require("./app");
 const dbConnection = require("./src/config/db/dbConnection");
+const port = Number(process.env.PORT) || 5000;
 //
 async function start() {
   try {
-    await fastify.listen({ port: process.env.PORT || 5000});
-    dbConnection()
+    await fastify.listen({ port: port });
+    dbConnection();
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
