93cff30add71a6baf747e45d576418a12e405aa9
Shakil <sakildevmern@gmail.com>
2025-03-24 23:40:00 +0600

    3/24/2025

--- src/handler/visitor/footer/getFooter.js
+++ src/handler/visitor/footer/getFooter.js
@@ -1,11 +1,11 @@
 "use strict";
-
+//
 const Footer = require("../../../model/footer/footer.model");
 
 //
 module.exports = async (req, reply) => {
   try {
-    const footer = await Footer.findOne();
+    const footer = await Footer.findOne({}, { _id: 0,});
     return reply.status(200).send({
       success: true,
       message: "Footer data fetched successfully",
