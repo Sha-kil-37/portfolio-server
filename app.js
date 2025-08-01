58b325a05723be5df746a13992b7b6dcd0422d86
Shakil <sakildevmern@gmail.com>
2025-08-02 00:31:24 +0600

    8/2/2025

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ fastify.register(require("@fastify/rate-limit"), {
 // REGISTER CORS
 fastify.register(require("@fastify/cors"), {
   // put your options here
-  origin: true, // origin er kaj ace 
+  origin: "*", // origin er kaj ace 
+
 });
 // REGISTER JWT TOKEN
 fastify.register(require("@fastify/jwt"), {
