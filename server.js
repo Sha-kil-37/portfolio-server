fb1d4db6318b0b57f3f42f2d9b0907bba187aeee
Shakil <sakildevmern@gmail.com>
2025-08-01 17:20:14 +0600

    8/1/2025

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 require("dotenv").config();
 const fastify = require("./app");
 const dbConnection = require("./src/config/db/dbConnection");
-const port = process.env.PORT || 8000;
+const port = Number(process.env.PORT) || 8000; // Default port is 8000 if not specified in .env
+
 //
 async function start() {
   try {
-    await fastify.listen({ port: port, host: "0.0.0.0" });
+    await fastify.listen({ port: 8000, host: "0.0.0.0" });
     dbConnection();
   } catch (error) {
     process.exit(1);
